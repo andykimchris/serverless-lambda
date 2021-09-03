9fba3d66faf6026115d965b40501874a9fed3477
andykimchris <andrew.muchiri97@gmail.com>
2021-09-03 19:33:01 +0300

    added fetchTodos function

---
+++ src/fetchTodos.js
@@ -0,0 +1,25 @@
+const AWS = require("aws-sdk")
+
+const fetchTodos = async event => {
+
+    const dynamodb = new AWS.DynamoDB.DocumentClient()
+    let todos;
+
+    try {
+        const results = await dynamodb.scan({ TableName: "TodoTable" }).promise()
+        todos = results.Items
+    } catch (error) {
+        console.log(error)
+    }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      todos
+    ),
+  };
+};
+
+module.exports = {
+  handler: fetchTodos
+}
